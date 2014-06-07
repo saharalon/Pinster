9e30a6830a6f480e0ce65d0f0dc624f66d667d6f
ido gold <goldido87@gmail.com>
2014-06-07 14:01:57 +0300

    Test Hyperlapse

--- www/js/PinsterApp.js
+++ www/js/PinsterApp.js
@@ -76,6 +76,8 @@ var PinsterApp = {
 
       that.setAppLanguage(that.fields.currentLanguage);
 
+      that.showHyperlapse();
+
     },  // END of onDocumentReady()
 
     // Update DOM on a Received Event
@@ -471,6 +473,48 @@ var PinsterApp = {
               directionsDisplay.setDirections(response);
             }
       });
+    },
+
+    showHyperlapse : function() {
+
+      var hyperlapse = new Hyperlapse(document.getElementById('pano'), {
+        lookat: new google.maps.LatLng(37.81409525128964,-122.4775045005249),
+        zoom: 1,
+        use_lookat: true,
+        elevation: 50
+      });
+
+      hyperlapse.onError = function(e) {
+        console.log(e);
+      };
+
+      hyperlapse.onRouteComplete = function(e) {
+        hyperlapse.load();
+      };
+
+      hyperlapse.onLoadComplete = function(e) {
+        hyperlapse.play();
+      };
+
+      // Google Maps API stuff here...
+      var directions_service = new google.maps.DirectionsService();
+
+      var route = {
+        request:{
+          origin: new google.maps.LatLng(37.816480000000006,-122.47825,37),
+          destination: new google.maps.LatLng(37.81195,-122.47773000000001),
+          travelMode: google.maps.DirectionsTravelMode.DRIVING
+        }
+      };
+
+      directions_service.route(route.request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          hyperlapse.generate( {route:response} );
+        } else {
+          console.log(status);
+        }
+      });
+
     },
 
     GoogleMap : function() {
