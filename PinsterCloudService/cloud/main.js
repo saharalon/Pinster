68ba1798577ec4bbdaf6e255e437c13be7bec7fe
Sahar Rehani <sahar.rehani@gmail.com>
2014-06-07 17:33:12 +0300

    commit for Eran

--- PinsterCloudService/cloud/main.js
+++ PinsterCloudService/cloud/main.js
@@ -4,3 +4,7 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("getRecommendedEvents", function(request, response) {
+  response.success([{title: "event1"},{title: "event2"},{title: "event3"}]);
+});
