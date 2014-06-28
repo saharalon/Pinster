454e584266b0adb8d634e7ae3dcba75b2bd8a74d
ido gold <goldido87@gmail.com>
2014-06-28 13:46:24 +0300

    Defined random event cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -5,6 +5,84 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-Parse.Cloud.define("getRecommendedEvents", function(request, response) {
-  response.success([{title: "event1"},{title: "event2"},{title: "event3"}]);
-});
+
+Parse.Cloud.define("getRecommendedEvent", function(request, response) {
+
+	var categories = new Array();
+	var addresses = new Array();
+
+	var data = request.params.data;
+
+	// Number each category and the times it has been searched
+	data.eventsCategory.forEach(function(item, index) {
+	if (!categories.hasOwnProperty(item))
+	{
+	  categories.push({key: item});
+	  categories[item] = 0;
+	}
+	categories[item]++;
+	});
+
+	// Sort from most searched category to least
+	categories.sort(function(a, b) {
+	  a = categories[a.key];
+	  b = categories[b.key];
+
+	  return a > b ? -1 : (a < b ? 1 : 0);
+	});
+
+	// Number each address and the times it has been searched
+	data.addresses.forEach(function(item, index) {
+	if (!addresses.hasOwnProperty(item))
+	{
+	  addresses.push({key: item});
+	  addresses[item] = 0;
+	}
+	addresses[item]++;
+	});
+
+	// Sort from most searched addresses to least
+	addresses.sort(function(a, b) {
+	  a = addresses[a.key];
+	  b = addresses[b.key];
+
+	  return a > b ? -1 : (a < b ? 1 : 0);
+	});
+
+	// Get one of the top 3 searched categories
+	var randomNum = Math.floor((Math.random() * 3) + 1);
+	var searchCategory = categories[randomNum - 1].key;
+
+	// Get one of the top 3 searched addresses
+	randomNum = Math.floor((Math.random() * 3) + 1);
+	var searchAddress = addresses[randomNum - 1].key;
+
+
+	var events = Parse.Object.extend("Event");
+	//set query for events objectr
+	var query = new Parse.Query(events);
+
+	//check the events within the specify point to search from
+	if (searchAddress != "")
+	query.withinKilometers("location", searchAddress, 10000);
+
+	// Add category parameter
+	if (searchCategory != undefined && searchCategory != "All")
+	query.equalTo("category", searchCategory);
+
+	query.find
+	({
+		success: function(results)
+		{
+			if (results.length > 0) 
+            {
+            	// Randomly pick one event
+			  	randomNum = Math.floor((Math.random() * results.length - 1) + 0);
+
+				// Return the event object       
+                response.success(JSON.stringify(results[randomNum]));
+            }
+		}
+	});
+
+}
--- www/js/User.js
+++ www/js/User.js
@@ -304,82 +304,24 @@ PinsterApp.User = function() {
     getSmartRandomEvent : function() {
 
       var data = JSON.parse(localStorage.getItem("pinsterSearches"));
-      var categories = new Array();
-      var addresses = new Array();
-
-      // Number each category and the times it has been searched
-      data.eventsCategory.forEach(function(item, index) {
-        if (!categories.hasOwnProperty(item))
-        {
-          categories.push({key: item});
-          categories[item] = 0;
-        }
-        categories[item]++;
-      });
-
-      // Sort from most searched category to least
-      categories.sort(function(a, b) {
-          a = categories[a.key];
-          b = categories[b.key];
-
-          return a > b ? -1 : (a < b ? 1 : 0);
-      });
-
-      // Number each address and the times it has been searched
-      data.addresses.forEach(function(item, index) {
-        if (!addresses.hasOwnProperty(item))
-        {
-          addresses.push({key: item});
-          addresses[item] = 0;
-        }
-        addresses[item]++;
-      });
-
-      // Sort from most searched addresses to least
-      addresses.sort(function(a, b) {
-          a = addresses[a.key];
-          b = addresses[b.key];
-
-          return a > b ? -1 : (a < b ? 1 : 0);
-      });
-
-      // Get one of the top 3 searched categories
-      var randomNum = Math.floor((Math.random() * 3) + 1);
-      var searchCategory = categories[randomNum - 1].key;
-
-      // Get one of the top 3 searched addresses
-      randomNum = Math.floor((Math.random() * 3) + 1);
-      var searchAddress = addresses[randomNum - 1].key;
-
-
-      var events = Parse.Object.extend("Event");
-      //set query for events objectr
-      var query = new Parse.Query(events);
-
-      //check the events within the specify point to search from
-      if (searchAddress != "")
-        query.withinKilometers("location", searchAddress, 10000);
       
-      // Add category parameter
-      if (searchCategory != undefined && searchCategory != "All")
-        query.equalTo("category", searchCategory);
+      var params = {};
+      params["data"] = data;
 
-      query.find({
-        success: function(placesObjects) {
+      // Call cloud function
+      Parse.Cloud.run('getRecommendedEvent', params, {
 
-          // Randomly pick one event
-          randomNum = Math.floor((Math.random() * placesObjects.length) + 1);
+        success: function(results) {
           
-          var selectedEvent = placesObjects[randomNum - 1]._serverData;
-          var eventId = placesObjects[randomNum].id;
-          var address = new google.maps.LatLng(
-            selectedEvent.location._latitude, selectedEvent.location._longitude);
+          var selectedEvent = JSON.parse(results);
+          var location = result._serverData;
+          var eventId = result.id;
+
+          var address = new google.maps.LatLng(location._latitude, location._longitude);
 
           // Focus on the selected event
           PinsterApp.fields.mapInstance.setCenter(address);
           
-          //obj.animateZoomIn(10);
-
           // Animate the selected event marker
           PinsterApp.fields.markers.forEach(function(marker, index) {
 
@@ -395,12 +337,16 @@ PinsterApp.User = function() {
                 }, 3000);
               }  
             }
-
           });
-        }
+        },
+
+        error: function(error) { }
+
       });
+
     }
 
+  // End of SearchData
   };
 
   obj.animateZoomIn = function(currentZoom)
